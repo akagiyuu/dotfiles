acfe5ce5c90b89a9ad6c687dc4129d9de7ebd4be
AkagiYuu <khanghuynh0245@gmail.com>
2023-03-17 14:51:40 +0700

    Use hyprland

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -6,3 +6,4 @@ alias n='nnn -eH'
 alias N='sudo -E nnn -deH'
 alias tree='erdtree'
 alias rustscan='docker run -it --rm --name rustscan rustscan/rustscan:latest'
+alias svn="svn --config-dir $XDG_CONFIG_HOME/subversion"
