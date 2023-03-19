88182b0ce5d115ac94aaefc21fbdf7dc88f45b4e
AkagiYuu <khanghuynh0245@gmail.com>
2023-03-19 08:34:59 +0700

    Update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,6 @@
 starship init fish | source
 zoxide init fish --cmd cd | source
+cod init %self fish | source
 if [ "$TERM_PROGRAM" = vscode ]
     source "/opt/visual-studio-code/resources/app/out/vs/workbench/contrib/terminal/browser/media/shellIntegration.fish"
 end
