c03da3d419072615a9b05c1a1700a3f261d91d36
AkagiYuu <khanghuynh0245@gmail.com>
2023-10-15 10:47:19 +0700

    feat: add yazi

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -3,6 +3,7 @@ alias ls='exa --icons -l'
 alias f='fzf'
 alias v='nvim'
 alias n='nnn -eH'
+alias y='yazi'
 alias rustscan='docker run -it --rm --name rustscan rustscan/rustscan:latest'
 alias svn="svn --config-dir $XDG_CONFIG_HOME/subversion"
 alias wget='wget --hsts-file="$XDG_DATA_HOME/wget-hsts"'
