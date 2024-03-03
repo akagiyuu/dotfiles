339874d164da5129a049e234504584fb27b1b135
akagiyuu <khanghuynh0245@gmail.com>
2024-03-03 11:45:05 +0700

    Update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -20,3 +20,7 @@ fish_add_path "$ARGC_COMPLETIONS_ROOT/bin"
 # To add a subset of completions only, change next line e.g. set argc_scripts cargo git
 set argc_scripts (ls -1 "$ARGC_COMPLETIONS_ROOT/completions" | sed -n 's/\.sh$//p')
 argc --argc-completions fish $argc_scripts | source
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
