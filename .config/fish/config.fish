16dde1121531510c9cf5760e93c9d83c2f594edd
akagiyuu <khanghuynh0245@gmail.com>
2024-09-01 16:42:14 +0700

    feat(fish): remove unused app

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,21 +7,6 @@ if status is-interactive
     pyenv virtualenv-init - | source
 end
 
-# tabtab source for packages
-# uninstall by removing these lines
-[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
-
-# opam configuration
-source /home/yuu/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
-
-# argc-completions
-set -gx ARGC_COMPLETIONS_ROOT "/home/yuu/.local/bin/argc-completions"
-set -gx ARGC_COMPLETIONS_PATH "$ARGC_COMPLETIONS_ROOT/completions"
-fish_add_path "$ARGC_COMPLETIONS_ROOT/bin"
-# To add a subset of completions only, change next line e.g. set argc_scripts cargo git
-set argc_scripts (ls -1 "$ARGC_COMPLETIONS_ROOT/completions" | sed -n 's/\.sh$//p')
-argc --argc-completions fish $argc_scripts | source
-
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
