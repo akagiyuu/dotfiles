d554b32f45df2d26843cae9727e76063e37e8944
akagiyuu <khanghuynh0245@gmail.com>
2024-12-22 10:36:28 +0700

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,14 +4,15 @@ zoxide init fish --cmd cd | source
 if status is-interactive
     colorscript random
     atuin init fish | source
-    pyenv virtualenv-init - | source
+    source ~/miniconda3/etc/fish/conf.d/conda.fish
+    # pyenv virtualenv-init - | source
 end
 
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 
-pyenv init - | source
+# pyenv init - | source
 
 # pnpm
 set -gx PNPM_HOME "/home/yuu/.local/share/pnpm"
