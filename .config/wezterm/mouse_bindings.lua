9e682df5362501c67dd464e60360b50579990ae5
akagiyuu <khanghuynh0245@gmail.com>
2025-01-04 10:40:23 +0700

    feat(wezterm): remove unused config

--- .config/wezterm/mouse_bindings.lua
+++
@@ -1,9 +0,0 @@
-local mouse_binding = require("utils").mouse_binding
-
-local config = {
-    mouse_binding("ctrl", "Left", "Up", 1, "OpenLinkAtMouseCursor"),
-    mouse_binding("ctrl", "Left", "Down", 1, "Nop"),
-}
-
-return config
-
--- .config/wezterm/ui/tab/init.lua
+++
@@ -1,4 +0,0 @@
-return {
-    normal = require(... .. '.normal'),
-    powerline = require(... .. '.powerline')
-}
